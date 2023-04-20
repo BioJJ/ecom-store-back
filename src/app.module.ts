8ae15fb8b3dcf554c1e4876f95928ad7230d92d7
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-04-20 02:05:19 -0400

    chore: 🔧 configuring database connection and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ProductsModule } from './products/products.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
+import { join } from 'path'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: '.env'
+		}),
+		ProductsModule,
+		TypeOrmModule.forRoot({
+			type: 'mongodb',
+			// url: process.env.MONGO_URL,
+			// database: process.env.MONGO_DATABASE,
+			// username: process.env.MONGO_USERNAME,
+			// password: process.env.MONGO_PASSWORD,
+			url: process.env.MONGO_CONNECT,
+			entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+			synchronize: true,
+			useNewUrlParser: true,
+			logging: true
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+
+	const config = new DocumentBuilder()
+		.setTitle('ECOM-STORE')
+		.setDescription('ECOM-STORE api')
+		.setVersion('1.0')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
+	app.useGlobalPipes(new ValidationPipe())
+	app.enableCors()
+
+	await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000)
 }
 bootstrap()
