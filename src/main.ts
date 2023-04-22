555919ecfe2f7ef3af5cdeadfd981460384c44d9
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-04-22 04:01:52 -0400

    refactor: ♻️ add pipes

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,13 @@ async function bootstrap() {
 
 	const document = SwaggerModule.createDocument(app, config)
 	SwaggerModule.setup('api', app, document)
-	app.useGlobalPipes(new ValidationPipe())
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			whitelist: true,
+			forbidNonWhitelisted: true
+		})
+	)
 	app.enableCors()
 
 	await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000)
