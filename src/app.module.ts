d440c7614d7d9519b31f4413d036aa9300d80e4b
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-04-22 03:55:42 -0400

    feat: ✨ add completed Auth Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { join } from 'path'
 import { AwsS3Module } from './aws-s3/aws-s3.module'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard'
 
 @Module({
 	imports: [
@@ -33,6 +35,12 @@ import { AuthModule } from './auth/auth.module'
 		AuthModule
 	],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: JwtAuthGuard
+		}
+	]
 })
 export class AppModule {}
